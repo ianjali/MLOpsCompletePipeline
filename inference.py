b90b829d3773642d5d47af0f17eca02cfe8e54ab
ianjali <amudgal26@gwmail.gwu.edu>
2025-03-18 13:47:36 +0530

    inference on cpu

--- inference.py
+++ inference.py
@@ -6,7 +6,9 @@ from data import DataModule
 class ColaPredictor:
     def __init__(self, model_path):
         self.model_path = model_path
-        self.model = ColaModel.load_from_checkpoint(model_path)
+        # self.model = ColaModel.load_from_checkpoint(model_path)
+        self.device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
+        self.model = ColaModel.load_from_checkpoint(model_path).to(self.device)
         self.model.eval()
         self.model.freeze()
         self.processor = DataModule()
@@ -17,8 +19,8 @@ class ColaPredictor:
         inference_sample = {"sentence": text}
         processed = self.processor.tokenize_data(inference_sample)
         logits = self.model(
-            torch.tensor([processed["input_ids"]]),
-            torch.tensor([processed["attention_mask"]]),
+            torch.tensor([processed["input_ids"]],device=self.device),
+            torch.tensor([processed["attention_mask"]],device=self.device),
         )
         scores = self.softmax(logits[0]).tolist()
         predictions = []
@@ -29,5 +31,5 @@ class ColaPredictor:
 
 if __name__ == "__main__":
     sentence = "The boy is sitting on a bench"
-    predictor = ColaPredictor("./models/epoch=0-step=267.ckpt")
+    predictor = ColaPredictor("./models/epoch=3-step=1072.ckpt")
     print(predictor.predict(sentence))
